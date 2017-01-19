146cbdd3b9c00be22d745a9f38fd7849e9f2b255
Terran <terrankroft@gmail.com>
2017-01-19 15:55:16 +0800

    All but TIME is fine 2

--- assets/js/time.js
+++ assets/js/time.js
@@ -29,7 +29,10 @@ function currentTime() {
 }
 
 function updateSchedule() {
-	$.getJSON("../../www/schedule.json", function(data) {
+	$.getJSON("classes.json", function(data) {
+        // for (thing in data) {
+        //     alert(thing)
+        // }
         document.getElementById("ms").textContent = getSchedule(data, "MS");
         document.getElementById("ms").textContent = getSchedule(data, "HS");
 	// document.getElementById("ms").textContent = new Schedule(data).currentBlock("MS");
@@ -53,17 +56,26 @@ function getSchedule(data, division) {
     // First check for custom days
     if (customSchedule in data) {
         todays_schedule = data[customSchedule];
-        alert("eh")
+        // alert("eh")
     } else {
         todays_schedule = data[dailySchedule]; // THIS HAS TO EXIST
-        alert(todays_schedule)
+        // alert(todays_schedule)
     }
-    var current_time = moment().format("HH:m");
+    // for (thing in todays_schedule) {
+    //     alert(thing);
+    // }
+    var current_time = moment().format("H:mm");
     var current_block;
     for (interval in todays_schedule) {
-        var startTime = moment(todays_schedule[interval][0], "HH:m");
-        var endTime = moment(todays_schedule[interval][1], "HH:m");
-        if (current_time.isSameOrAfter(startTime) && currentTime.isBefore(endTime)) {
+
+        var startTime = moment(todays_schedule[interval][0], "H:mm");
+        var endTime = moment(todays_schedule[interval][1], "H:mm");
+        // alert((current_time));
+        // alert(moment(startTime).format("H:mm") + " until " + moment(endTime).format("H:mm"));
+        alert(moment(current_time).isBetween(startTime, endTime, 'minute'))
+        // alert(startTime + 'until' + endTime + "int: " + interval)
+        // alert(moment(current_time).isSameOrAfter(startTime) + "and " + moment(currentTime).isBefore(endTime))
+        if (moment(current_time).isSameOrAfter(startTime) && moment(currentTime).isBefore(endTime)) {
             current_block = interval;
             alert("OH " + interval)
             break;
@@ -77,180 +89,35 @@ function getSchedule(data, division) {
     //with a bell schedule object, now we want to see what time range it falls between
 }
 
-function Schedule(schedule) {
-	this.schedule = schedule;
+// function Schedule(schedule) {
+// 	this.schedule = schedule;
 
-	this.currentBlock = function(division) {
-		var today = new Date();
-		// only update schedule once a minute
-
-
-
-		var dayScheduleType = dayOfWeek[today.getDay()] + division;
-		var block;
-		if(dayScheduleType in schedule) {
-			//if it's on saturday or not recognized
-			var jsonSchedule = schedule[dayScheduleType];
-			for (var interval in schedule[dayScheduleType]) {
-			if (today >= parseTimeString(jsonSchedule[interval][0]) && today < parseTimeString(jsonSchedule[interval][1])) {
-				// Check if the current time is within range
-				block = interval;
-				break;
-			}
-		}
-		} 
-
-		if(!block) {
-			//if it's not a block 
-			block = "--";
-		}
-		return block;
-	}
-}
+// 	this.currentBlock = function(division) {
+// 		var today = new Date();
+// 		// only update schedule once a minute
 
 
 
+// 		var dayScheduleType = dayOfWeek[today.getDay()] + division;
+// 		var block;
+// 		if(dayScheduleType in schedule) {
+// 			//if it's on saturday or not recognized
+// 			var jsonSchedule = schedule[dayScheduleType];
+// 			for (var interval in schedule[dayScheduleType]) {
+// 			if (today >= parseTimeString(jsonSchedule[interval][0]) && today < parseTimeString(jsonSchedule[interval][1])) {
+// 				// Check if the current time is within range
+// 				block = interval;
+// 				break;
+// 			}
+// 		}
+// 		} 
 
-
-
-
-// var schedule = {
-// 	"mondayHS": {
-// 		"A": 		["8:00", "8:50"],
-// 		"A>B": 		["8:50", "8:55"],
-// 		"B": 		["8:55", "9:45"],
-// 		"B>E": 		["9:45", "9:50"],
-// 		"E": 		["9:50", "10:40"],
-// 		"Break": 	["10:40", "10:55"],
-// 		"D": 		["10:55", "11:45"],
-// 		"D>C": 		["11:45", "11:50"],
-// 		"C": 		["11:50", "12:40"],
-// 		"Lunch": 	["12:40", "13:15"],
-// 		"F": 		["13:15", "14:05"],
-// 		"F>G": 		["14:05", "14:10"],
-// 		"G": 		["14:10", "15:00"]
-// 	},
-// 	"mondayMS": {
-// 		"A": 		["8:00", "8:50"],
-// 		"A>B": 		["8:50", "8:55"],
-// 		"B": 		["8:55", "9:45"],
-// 		"B>E": 		["9:45", "9:50"],
-// 		"E": 		["9:50", "10:40"],
-// 		"Break": 	["10:40", "10:55"],
-// 		"D": 		["10:55", "11:45"],
-// 		"Lunch": 	["11:45", "12:20"],
-// 		"C": 		["12:20", "13:10"],
-// 		"C>F": 		["13:10", "13:15"],
-// 		"F": 		["13:15", "14:05"],
-// 		"F>G": 		["14:05", "14:10"],
-// 		"G": 		["14:10", "15:00"]
-// 	},
-// 	"tuesdayHS": {
-// 		"B": 		["8:00", "9:30"],
-// 		"Break": 	["9:30", "9:45"],
-// 		"D": 		["9:45", "11:15"],
-// 		"D>C": 		["11:15", "11:20"],
-// 		"C": 		["11:20", "12:50"],
-// 		"Lunch": 	["12:50", "13:25"],
-// 		"Lunch>E":	["13:25", "13:30"],
-// 		"E": 		["13:30", "15:00"]
-// 	},
-// 	"tuesdayMS": {
-// 		"B": 		["8:00", "9:30"],
-// 		"Break": 	["9:30", "9:45"],
-// 		"D": 		["9:45", "11:15"],
-// 		"Lunch":	["11:15", "11:50"],
-// 		"Lunch>C":	["11:50", "11:55"],
-// 		"C": 		["11:55", "13:25"],
-// 		"C>E":		["13:25", "13:30"],
-// 		"E": 		["13:30", "15:00"]
-// 	},
-// 	"wednesdayHS": {
-// 		"F": 		["8:00", "9:30"],
-// 		"Break": 	["9:30", "9:45"],
-// 		"G": 		["9:45", "11:15"],
-// 		"G>Flex":	["11:15", "11:20"],
-// 		"Flex": 	["11:20", "12:05"],
-// 		"Flex>Adv": ["12:05", "12:10"],
-// 		"Advisory": ["12:10", "12:50"],
-// 		"Lunch": 	["12:50", "13:25"],
-// 		"Lunch>A":	["13:25", "13:30"],
-// 		"A": 		["13:30", "15:00"]
-// 	},
-// 	"wednesdayMS": {
-// 		"F": 		["8:00", "9:30"],
-// 		"Break": 	["9:30", "9:45"],
-// 		"G": 		["9:45", "11:15"],
-// 		"G>Adv": 	["11:15", "11:20"],
-// 		"Advisory": ["11:20", "12:05"],
-// 		"Lunch": 	["12:05", "12:40"],
-// 		"Lunch>Flx":["12:40", "12:45"],
-// 		"Flex": 	["12:45", "13:25"],
-// 		"Flex>A":	["13:25", "13:30"],
-// 		"A": 		["13:30", "15:00"]
-// 	},
-// 	"thursdayHS": {
-// 		"B": 		["8:00", "8:50"],
-// 		"B>D": 		["8:50", "8:55"],
-// 		"D": 		["8:55", "9:45"],
-// 		"D>E": 		["9:45", "9:50"],
-// 		"E": 		["9:50", "10:40"],
-// 		"Break": 	["10:40", "10:55"],
-// 		"F": 		["10:55", "11:45"],
-// 		"F>G": 		["11:45", "11:50"],
-// 		"G": 		["11:50", "12:40"],
-// 		"Lunch": 	["12:40", "13:15"],
-// 		"A": 		["13:15", "14:05"],
-// 		"A>C": 		["14:05", "14:10"],
-// 		"C": 		["14:10", "15:00"]
-// 	},
-// 	"thursdayMS": {
-// 		"B": 		["8:00", "8:50"],
-// 		"B>D": 		["8:50", "8:55"],
-// 		"D": 		["8:55", "9:45"],
-// 		"D>E": 		["9:45", "9:50"],
-// 		"E": 		["9:50", "10:40"],
-// 		"Break": 	["10:40", "10:55"],
-// 		"F": 		["10:55", "11:45"],
-// 		"Lunch": 	["11:45", "12:20"],
-// 		"G": 		["12:20", "13:10"],
-// 		"G>A": 		["13:10", "13:15"],
-// 		"A": 		["13:15", "14:05"],
-// 		"A>C": 		["14:05", "14:10"],
-// 		"C": 		["14:10", "15:00"]
-// 	},
-// 	"fridayHS": {
-// 		"D": 		["8:00", "8:50"],
-// 		"D>E": 		["8:50", "8:55"],
-// 		"E": 		["8:55", "9:45"],
-// 		"E>F": 		["9:45", "9:50"],
-// 		"F": 		["9:50", "10:40"],
-// 		"Break": 	["10:40", "10:55"],
-// 		"G": 		["10:55", "11:45"],
-// 		"G>A": 		["11:45", "11:50"],
-// 		"A": 		["11:50", "12:40"],
-// 		"Lunch": 	["12:40", "13:15"],
-// 		"C": 		["13:15", "14:05"],
-// 		"C>B": 		["14:05", "14:10"],
-// 		"B": 		["14:10", "15:00"]
-// 	},
-// 	"fridayMS": {
-// 		"D": 		["8:00", "8:50"],
-// 		"D>E": 		["8:50", "8:55"],
-// 		"E": 		["8:55", "9:45"],
-// 		"E>F": 		["9:45", "9:50"],
-// 		"F": 		["9:50", "10:40"],
-// 		"Break": 	["10:40", "10:55"],
-// 		"G": 		["10:55", "11:45"],
-// 		"Lunch": 	["11:45", "12:20"],
-// 		"G": 		["12:20", "13:10"],
-// 		"G>C": 		["13:10", "13:15"],
-// 		"C": 		["13:15", "14:05"],
-// 		"C>B": 		["14:05", "14:10"],
-// 		"B": 		["14:10", "15:00"]
+// 		if(!block) {
+// 			//if it's not a block 
+// 			block = "--";
+// 		}
+// 		return block;
 // 	}
-
-
 // }
 
 
